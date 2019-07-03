869dcedb2a171ec45711d04abb8108489301211a
Zhen Wang <zhenw2676@163.com>
2019-07-02 23:07:13 -0700

    pure annotation

--- spring-zhange/src/main/java/com/kkb/spring/test/TestSpringIoC.java
+++ spring-zhange/src/main/java/com/kkb/spring/test/TestSpringIoC.java
@@ -2,21 +2,23 @@ package com.kkb.spring.test;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.kkb.spring.configuration.SpringConfiguration;
 import com.kkb.spring.service.UserService;
 
 public class TestSpringIoC {
 
 	@Test
 	public void test1() {
-		//创建容器
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		//根据Bean的类型，从容器中获取实例
-		UserService service1 = context.getBean(UserService.class);
-		//UserService service2 = (UserService) context.getBean("userServiceImpl");
+		// 创建纯注解方式的spring容器
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
 		
-		service1.saveUser();
-		//service2.saveUser();
+		//从容器中获取Bean的实例
+		UserService service = context.getBean(UserService.class);
+		service.saveUser();
+		
+		UserService service2 = (UserService) context.getBean("userService");
+		service2.saveUser();
 	}
 }
